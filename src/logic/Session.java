b942cb00a8b74a67e68d3c63b6684802e45a166c
CameronNicolson <cameronnicolson50@gmail.com>
2018-01-24 01:03:44 +0000

    Sessions class is now a HashMap with keys pointing to value of Game objects

--- src/logic/Session.java
+++ src/logic/Session.java
@@ -1,18 +1,31 @@
 package logic;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Session {
-	private static ArrayList<Game> gameList = new ArrayList<Game>();  
+	private static HashMap<Integer, Game> gameList = new HashMap<Integer, Game>();  
+	private static Session instance = null;
+	private static int counter = 1;
+	
+	private Session() {
+		
+	}
+	
+	public static Session getInstance() {
+		if(instance == null) {
+			instance = new Session();
+		 }
+		return instance;
+	}
 	
 	public static Game createNewGame(int aiSelect) {
 		Game newGame = new Game(aiSelect);
-		gameList.add(newGame);
+		gameList.put(counter, newGame);
+		counter++;
 		return newGame;
 	}
 
 	public static Game findGameById(int gameId) {
-		// loop through game list
-		return null;
+			return gameList.get(gameId);
 	}
 }
