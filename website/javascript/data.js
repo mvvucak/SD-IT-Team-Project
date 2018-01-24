9c8cad112c14d1c84786d58de5f0be158d72b823
CameronNicolson <cameronnicolson50@gmail.com>
2018-01-24 12:21:11 +0000

    Mock JSON data

---
+++ website/javascript/data.js
@@ -0,0 +1,28 @@
+ var globalData = {
+	"options": {
+		"aiAvailable": 4
+	},
+"stats": [
+	{
+	"gameId": 1,	
+	"moves": 44,
+	"ties-won": 22,
+	"draws-won": 33, 
+	"human-winner": true
+	},
+	{
+	"gameId": 2,	
+	"moves": 87,
+	"ties-won": 22,
+	"draws-won": 77, 
+	"human-winner": false
+	},
+	{
+	"gameId": 3,	
+	"moves": 100,
+	"ties-won": 33,
+	"draws-won": 32, 
+	"human-winner": true
+	}
+]
+}
