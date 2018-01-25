3560bc1cab00e0e936f57d3db90dc4520490d866
mvvucak <wepster34@hotmail.com>
2018-01-25 20:17:09 +0000

    Category Choice Menu
    Added method to Command Line controller to prompt the user for a choice of category.
     The method was transferred over from the HumanPlayer class.
     chooseCategory() is no longer an abstract method in Player.java.
     chooseCategory() still works normally for the AI.

--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -52,6 +52,43 @@ public class TopTrumpsCLIApplication {
 		scan.close();
 	}
 	
+	
+	/**
+	 * Asks the human user which category they would like to choose for a round
+	 * Only called when it is the human player's turn.
+ 	 * @return The name of the chosen category. NOTE: can be changed to return the index of
+ 	 * a category as an int.
+	 */
+	private String promptCategoryChoice()
+	{
+		System.out.println("1. Size");
+		System.out.println("2. Speed");
+		System.out.println("3. Range");
+		System.out.println("4. Firepower");
+		System.out.println("5. Cargo");
+		
+		int category = 0;
+		Scanner in = new Scanner(System.in);
+		
+		while(category < 1 || category > 5)
+		{
+			System.out.print("Choose a category: ");
+			String line = in.nextLine();
+			try
+			{
+				category = Integer.parseInt(line);
+			}
+			catch(NumberFormatException e)
+			{
+				System.out.println(line + " is not a valid input. Choose a number between 1 and 5.");
+			}
+			
+		}
+		in.close();
+		
+		return Card.catNames[category-1];
+	}
+	
 	private static boolean validChoice(int choice, int[] answers) {
 	    return ArrayUtils.contains(answers, choice);
 	}
